c68585da7c5ccf1e990d994b840ad708317f0a58
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-02-24 14:52:44 +0000

    Added links and styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,23 @@
 import React from "react";
+import "../styles/NavBar.css";
+import logo from "../images/logo.png";
 
 const NavBar = () => {
-  return <div />;
+  return (
+    <div className="navbar">
+      <img src={logo} alt="Logo" />
+      <div className="navbar-links">
+        <ul>
+          <li className="navbar-links-item">
+            <a href="www.google.com">View properties</a>
+          </li>
+          <li className="navbar-links-item">
+            <a href="www.google.com">Add a property</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
