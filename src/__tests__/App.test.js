3d52af71609861e8d9e8f92e4925f4580960624a
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-03-02 10:27:59 +0000

    Updated tests to incorporate use of React Router - followed React Testing Library documentation and installed history module

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,15 +1,27 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import App from "../components/App";
 
 describe("App", () => {
   it("renders as expected", () => {
-    const { asFragment } = render(<App />);
+    const history = createMemoryHistory();
+    const { asFragment } = render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders property app text", () => {
-    render(<App />);
+    const history = createMemoryHistory();
+    render(
+      <Router history={history}>
+        <App />
+      </Router>
+    );
     const appName = screen.getByText("Property App");
     expect(appName).toBeInTheDocument();
   });
--- src/__tests__/NavBar.test.js
+++ src/__tests__/NavBar.test.js
@@ -1,21 +1,38 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
 import NavBar from "../components/NavBar";
 
 describe("NavBar", () => {
   it("renders as expected", () => {
-    const { asFragment } = render(<NavBar />);
+    const history = createMemoryHistory();
+    const { asFragment } = render(
+      <Router history={history}>
+        <NavBar />
+      </Router>
+    );
     expect(asFragment()).toMatchSnapshot();
   });
 
   it("renders logo", () => {
-    const { getByTestId } = render(<NavBar />);
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Router history={history}>
+        <NavBar />
+      </Router>
+    );
     const logo = getByTestId("logo");
     expect(logo).toBeTruthy();
   });
 
   it("renders nav links", () => {
-    const { getByTestId } = render(<NavBar />);
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Router history={history}>
+        <NavBar />
+      </Router>
+    );
     const navLink1 = getByTestId("nav-link-1");
     const navLink2 = getByTestId("nav-link-2");
     expect(navLink1).toBeTruthy();
