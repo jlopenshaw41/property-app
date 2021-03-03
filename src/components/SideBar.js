7098b478d3761607dbbd5a7c305c3bcaa1ea3009
Jennifer Openshaw <jen@jenniferopenshaw.dev>
2021-03-03 13:41:53 +0000

    Refactored SideBar to place useLocation hook inside component

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,9 +2,7 @@ import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import qs from "qs";
 
-const buildQueryString = (operation, valueObj) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { search } = useLocation();
+const buildQueryString = (operation, valueObj, search) => {
   const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
 
   const newQueryParams = {
@@ -12,37 +10,50 @@ const buildQueryString = (operation, valueObj) => {
     [operation]: JSON.stringify(valueObj),
   };
 
-  return qs.stringify(newQueryParams, { addQueryPrefix: true, encode: false });
+  return qs.stringify(newQueryParams, {
+    addQueryPrefix: true,
+    encode: false,
+  });
 };
 
 const SideBar = () => {
+  const { search } = useLocation();
   return (
     <div>
       <Link
         className="link"
-        to={buildQueryString("query", { city: "Manchester" })}
+        to={buildQueryString("query", { city: "Manchester" }, search)}
       >
         Manchester
       </Link>
-      <Link className="link" to={buildQueryString("query", { city: "Leeds" })}>
+      <Link
+        className="link"
+        to={buildQueryString("query", { city: "Leeds" }, search)}
+      >
         Leeds
       </Link>
       <Link
         className="link"
-        to={buildQueryString("query", { city: "Sheffield" })}
+        to={buildQueryString("query", { city: "Sheffield" }, search)}
       >
         Sheffield
       </Link>
       <Link
         className="link"
-        to={buildQueryString("query", { city: "Liverpool" })}
+        to={buildQueryString("query", { city: "Liverpool" }, search)}
       >
         Liverpool
       </Link>
-      <Link className="link" to={buildQueryString("sort", { price: -1 })}>
+      <Link
+        className="link"
+        to={buildQueryString("sort", { price: -1 }, search)}
+      >
         Sort by price (lowest to highest)
       </Link>
-      <Link className="link" to={buildQueryString("sort", { price: 1 })}>
+      <Link
+        className="link"
+        to={buildQueryString("sort", { price: 1 }, search)}
+      >
         Sort by price (highest to lowest)
       </Link>
     </div>
